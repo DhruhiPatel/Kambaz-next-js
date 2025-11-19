205e41ad0ac5adece9e96bab7b86aa0a8897c963
Dhruhi <patel.dhruh@northeastern.edu>
2025-11-20 02:16:10 +0530

    secondcommit

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
+{/*import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const USERS_API = `${HTTP_SERVER}/api/users`;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post( `${USERS_API}/signin`, credentials );
   return response.data;
@@ -13,7 +12,6 @@ export const profile = async () => {
   return response.data;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const signup = async (user: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
   return response.data;
@@ -22,12 +20,65 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export const updateUser = async (user: any) => {
   const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
   return response.data;
 };
+*/}
 
 
-
-
+ import axios from "axios";
+ 
+// Basic types so ESLint stops complaining
+export interface Credentials {
+  email: string;
+  password: string;
+}
+ 
+export interface User {
+  _id?: string;
+  email: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+}
+ 
+const axiosWithCredentials = axios.create({ withCredentials: true });
+ 
+export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+export const USERS_API = `${HTTP_SERVER}/api/users`;
+ 
+export const signin = async (credentials: Credentials) => {
+  const response = await axiosWithCredentials.post(
+    `${USERS_API}/signin`,
+    credentials
+  );
+  return response.data;
+};
+ 
+export const profile = async () => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  return response.data;
+};
+ 
+export const signup = async (user: User) => {
+  const response = await axiosWithCredentials.post(
+    `${USERS_API}/signup`,
+    user
+  );
+  return response.data;
+};
+ 
+export const signout = async () => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
+  return response.data;
+};
+ 
+export const updateUser = async (user: User) => {
+  const response = await axiosWithCredentials.put(
+    `${USERS_API}/${user._id}`,
+    user
+  );
+  return response.data;
+};
